e4f1078b195b2e99edb1d59816bf7d51bd59df86
Michal Stepanek <micstepanek@gmail.com>
2020-11-10 13:58:19 +0100

    Merge restart and end timer scipts

--- end_of_timer.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo end of timer
-echo hit Enter to start another 20 minutes 
-read
-systemctl --user restart eye.timer
--- restart_eye_timer.sh
+++ restart_eye_timer.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-systemctl --user restart eye.timer
+systemctl --user stop eye.timer
+echo End of timer.
+echo Hit Enter to start another 20 minutes.
+read
+systemctl --user start eye.timer
